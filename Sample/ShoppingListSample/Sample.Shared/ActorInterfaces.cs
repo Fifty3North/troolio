5100ab0d5b7a93318f674c7c4a34d5d07a9217e3
Richard Pickering <richard@fifty3north.com>
2022-05-27 11:17:29 +0100

    F3N-149 cleanup

--- Sample/ShoppingListSample/Sample.Shared/ActorInterfaces.cs
+++ Sample/ShoppingListSample/Sample.Shared/ActorInterfaces.cs
@@ -11,3 +11,4 @@ public interface IUserActor : IStatefulActor, IActorGrain, IGrainWithStringKey {
 public interface IShoppingListActor : ICreatableActor, IStatefulActor, IActorGrain, IGrainWithStringKey { }
 public interface IShoppingListItemEFProjection : IActorGrain, IGrainWithStringKey { }
 public interface IShoppingListEFProjection : IActorGrain, IGrainWithStringKey { }
+public interface IShoppingReadModelOrchestrator : IActorGrain, IGrainWithStringKey { }
--- Sample/ShoppingListSample/Sample.Shared/ReadModels/ReadModelOrchestrator.cs
+++ Sample/ShoppingListSample/Sample.Shared/ReadModels/ReadModelOrchestrator.cs
@@ -1,31 +1,28 @@
-﻿using Orleankka;
-using Orleans;
+﻿using Orleans;
+using Sample.Shared.ActorInterfaces;
 using Sample.Shared.Events;
 using Troolio.Core;
 using Troolio.Core.ReadModels;
 using Troolio.Stores;
 
-namespace Sample.Shared.ReadModels
+namespace Sample.Shared.ReadModels;
+
+[RegexImplicitStreamSubscription("ShoppingListActor-.*")]
+[RegexImplicitStreamSubscription("AllShoppingListsActor-.*")]
+public class ShoppingReadModelOrchestrator 
+    : ReadModelOrchestrator<ShoppingList>, IShoppingReadModelOrchestrator, IGrainWithGuidCompoundKey
 {
-    public interface IShoppingReadModelOrchestrator : IActorGrain, IGrainWithStringKey { }
+    public ShoppingReadModelOrchestrator(IStore store) : base(store) { }
 
-    [RegexImplicitStreamSubscription("ShoppingListActor-.*")]
-    [RegexImplicitStreamSubscription("AllShoppingListsActor-.*")]
-    public class ShoppingReadModelOrchestrator 
-        : ReadModelOrchestrator<ShoppingList>, IShoppingReadModelOrchestrator, IGrainWithGuidCompoundKey
-    {
-        public ShoppingReadModelOrchestrator(IStore store) : base(store) { }
+    public string Handle(EventEnvelope<NewListCreated> ev) 
+        => ev.Id.ToString();
+    public string Handle(EventEnvelope<ItemAddedToList> ev) 
+        => ev.Id.ToString();
+    public string Handle(EventEnvelope<ItemRemovedFromList> ev) 
+        => ev.Id.ToString();
+    public string Handle(EventEnvelope<ListJoined> ev) 
+        => ev.Id.ToString();
+    public string Handle(EventEnvelope<ShoppingListAdded> ev) 
+        => ev.Event.ListId.ToString();
 
-        public string Handle(EventEnvelope<NewListCreated> ev) 
-            => ev.Id.ToString();
-        public string Handle(EventEnvelope<ItemAddedToList> ev) 
-            => ev.Id.ToString();
-        public string Handle(EventEnvelope<ItemRemovedFromList> ev) 
-            => ev.Id.ToString();
-        public string Handle(EventEnvelope<ListJoined> ev) 
-            => ev.Id.ToString();
-        public string Handle(EventEnvelope<ShoppingListAdded> ev) 
-            => ev.Event.ListId.ToString();
-
-    }
 }

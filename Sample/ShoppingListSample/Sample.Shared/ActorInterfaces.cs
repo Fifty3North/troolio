f744f336b674b08f475cbe86cbf739c388eb405e
Richard Pickering <richard@fifty3north.com>
2022-06-07 10:01:07 +0100

    F3N-162 remove unrequired references to simplify user understanding

--- Sample/ShoppingListSample/Sample.Shared/ActorInterfaces.cs
+++ Sample/ShoppingListSample/Sample.Shared/ActorInterfaces.cs
@@ -7,7 +7,7 @@ namespace Sample.Shared.ActorInterfaces;
 public interface IAllShoppingListsActor : IStatefulActor { }
 public interface IEmailActor : IActor { }
 public interface IUserActor : IStatefulActor { }
-public interface IShoppingListActor : ICreatableActor, IStatefulActor { }
+public interface IShoppingListActor : ICreatableActor { }
 public interface IShoppingListItemEFProjection : IActor { }
 public interface IShoppingListEFProjection : IActor { }
 public interface IShoppingReadModelOrchestrator : IActor { }
